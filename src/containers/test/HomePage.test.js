78963b686ec7c3517748a32ca0a43c18ab334bdf
Aldemaro González <aldemarogonzalez@Aldemaros-MacBook-Pro.local>
2021-04-10 22:09:40 +0200

    finish

--- src/containers/test/HomePage.test.js
+++ src/containers/test/HomePage.test.js
@@ -1,61 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { fetchPost } from "./api";
+import { render, fireEvent } from '@testing-library/react'
 
-export default function App2() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/post/:postId" component={Post} />
-      </Switch>
-    </Router>
-  );
-}
+import { HomePage } from "../HomePage";
+import { RoundDetailsPage } from "../RoundDetailsPage";
 
-function Home({ history }) {
-  const [postId, setPostId] = React.useState("");
-  return (
-    <div>
-      <h1>Welcome!</h1>
-      <h2>Search for a post by its ID</h2>
-
-      <label htmlFor="postId">Post ID: </label>
-      <input
-        id="postId"
-        value={postId}
-        onChange={e => setPostId(e.target.value)}
-      />
-      <button
-        disabled={!postId}
-        onClick={() => history.push(`/post/${postId}`)}
-      >
-        Submit
-      </button>
-    </div>
-  );
-}
-
-function Post({ match }) {
-  const { postId } = match.params;
-  const [post, setPost] = React.useState();
-  React.useEffect(() => {
-    (async function fetch() {
-      setPost(await fetchPost(postId));
-    })();
-  }, [postId]);
-  return (
-    <div>
-      <h1>Post {postId}</h1>
-      {!post ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </>
-      )}
-      <Link to="/">Back to Home</Link>
-    </div>
-  );
+test("allows user to consult campaign details", () => {
+	const { getByText, getByLabelText } = render(<HomePage/>);
+	const goToCampaign = getByLabelText("Ver Detalles");
+	const campaignDetails = fireEvent.click(goToCampaign);
+	expect(campaignDetails.toBe(RoundDetailsPage))
 }
